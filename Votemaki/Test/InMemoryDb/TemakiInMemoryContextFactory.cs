dfe15c03473bc488268432369f81fc47a7240a48
fjlimal <fjlimal@gmail.com>
2020-07-05 17:41:19 -0300

    Inserted In memory Sqlite DB for teste and some tests added to BaseReposiotry Crud

---
+++ Votemaki/Test/InMemoryDb/TemakiInMemoryContextFactory.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+using Votemaki.Infra.Storage;
+
+namespace Infra.Test.InMemoryDb
+{
+    public class TemakiInMemoryContextFactory : IDisposable
+    {
+        private DbConnection _connection;
+
+        private DbContextOptions<TemakiContext> CreateOptions()
+        {
+            return new DbContextOptionsBuilder<TemakiContext>()
+                .UseSqlite(_connection).Options;
+        }
+
+        public TemakiContext CreateContext()
+        {
+            if (_connection == null)
+            {
+                _connection = new SqliteConnection("DataSource=:memory:");
+                _connection.Open();
+
+                var options = CreateOptions();
+                using (var context = new TemakiContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+            }
+
+            return new TemakiContext(CreateOptions());
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null)
+            {
+                _connection.Dispose();
+                _connection = null;
+            }
+        }
+    }
+}
---
+++ Votemaki/Test/Repositories/BaseRepositoryTest.cs
@@ -0,0 +1,163 @@
+﻿using Infra.Test.InMemoryDb;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Votemaki.Core.Entities.ConfigurationEntities;
+using Votemaki.Core.Entities.MainEntities;
+using Votemaki.Infra.Repositories;
+using Votemaki.Infra.Storage;
+using Xunit;
+
+namespace Infra.Test.Repositories
+{
+    public class BaseRepositoryTest
+    {
+
+
+        [Fact]
+        public async Task TestAddAsync()
+        {
+            var institute = createInstitutionInstance();
+            using (var factory = new TemakiInMemoryContextFactory())
+            {
+                using (var context = factory.CreateContext())
+                {
+                    var instituteRepository = new InstitutionRepository(context);
+                    await instituteRepository.AddAsync(institute);
+                    await context.SaveChangesAsync();
+                }
+
+                using (var context = factory.CreateContext())
+                {
+                    var success = await context.Institutions.AnyAsync(
+                        i1 => (i1.Address == institute.Address) && (i1.Name == institute.Name)
+                        );
+
+                    Assert.True(success);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task TestGetAsync()
+        {
+            var institute = createInstitutionInstance();
+            using (var factory = new TemakiInMemoryContextFactory())
+            {
+                var id = await SaveInstitute(factory, institute);
+
+
+                using (var context = factory.CreateContext())
+                {
+                    var repository = new InstitutionRepository(context);
+                    var institionGotten = await repository.GetAsync(id);
+
+
+                    Assert.True(
+                        compareInstitutes(institute, institionGotten)
+                        );
+                }
+            }
+        }
+
+        [Fact]
+        public async Task TestUpdateAsync()
+        {
+            var institute = createInstitutionInstance();
+            using (var factory = new TemakiInMemoryContextFactory())
+            {
+                var id = await SaveInstitute(factory, institute);
+
+                var institution2 = new Institution()
+                {
+                    Id = id,
+                    Name = "minha casa",
+                    Address = "nossa casa"
+                };
+                using (var context = factory.CreateContext())
+                {
+                    var repository = new InstitutionRepository(context);
+
+                   
+
+                    await repository.UpdateAsync(institution2);
+                }
+
+                var institutionGotten = await GetInstitution(factory, id);
+
+                Assert.True(
+                    compareInstitutes(institution2, institutionGotten)
+                    );
+            }
+        }
+
+        [Fact]
+        public async Task TestDeleteAsync()
+        {
+            var institute = createInstitutionInstance();
+            using (var factory = new TemakiInMemoryContextFactory())
+            {
+                var id = await SaveInstitute(factory, institute);
+
+
+                using (var context = factory.CreateContext())
+                {
+                    var repository = new InstitutionRepository(context);
+
+
+
+                    await repository.DeleteAsync(id);
+                }
+
+                var institutionGotten = await GetInstitution(factory, id);
+
+                Assert.True(
+                    institutionGotten is null
+                    );
+            }
+        }
+
+
+
+
+
+        private bool compareInstitutes(Institution i1, Institution i2)
+        {
+            return (i1.Address == i2.Address) && (i1.Name == i2.Name);
+        }
+
+        private static async Task<Guid> SaveInstitute(TemakiInMemoryContextFactory ctxf, Institution i)
+        {
+            using (var ctx = ctxf.CreateContext())
+            {
+                await ctx.AddAsync<Institution>(i);
+                await ctx.SaveChangesAsync();
+                return i.Id;
+            }
+        }
+
+        private static async Task<Institution> GetInstitution(TemakiInMemoryContextFactory fac, Guid id)
+        {
+            using (var context = fac.CreateContext())
+            {
+                return await context.FindAsync<Institution>(id);
+            }
+        }
+
+        private static Institution createInstitutionInstance()
+        {
+
+            return new Institution()
+            {
+                Address = "Quadra um",
+                Name = "Institution",
+                ProcessConfigurations = new List<ProcessConfiguration>(),
+                Regions = new List<Region>()
+            };
+        }
+    }
+
+}
